b1f565e929cc4361e942af194f4ec69f2075c486
see0928 <see0928@naver.com>
2020-10-24 17:32:51 +0900

    Create customer.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,54 @@
 import React from 'react';
-import logo from './logo.svg';
+import React, {Componet} from 'react';
 import './App.css';
+import Customer from './components/Customer';
 
-function App() {
-  return (
-    <div className="gray-background">
-      <img src ={logo} lat ="logo"/>
-       <h2>Let's develop management system!</h2> 
-    </div>
-  );
+const customers =[{
+  'id' : 1,
+  'image' : ' https://placeing.com/64/64/any',
+  'name' : '나동빈',
+  'birthday' : '961221',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id' : 2,
+  'image' : ' https://placeing.com/64/64/any',
+  'name' : '나동빈',
+  'birthday' : '961221',
+  'gender' : '남자',
+  'job' : '대학생'
+},
+{
+  'id' : 3,
+  'image' : ' https://placeing.com/64/64/any',
+  'name' : '나동빈',
+  'birthday' : '961221',
+  'gender' : '남자',
+  'job' : '대학생'
+}]
+
+class App extends Component() {
+  render() {
+    return(
+      <div>
+      {
+        customers.map(c=> {
+          return(
+          <Customer
+          key={c.id}
+          id={c.id}
+          image={c.image}
+          name={c.name}
+          gender={c.gender}
+          job={c.job}     
+          />
+          );
+          })
+        }
+      </div> 
+          );  
+  }
 }
 
 export default App;
--- server.js
+++ server.js
@@ -6,8 +6,21 @@ const port = process.env.Port || 5000;
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended :true}));
 
-app.get('/api/hello',(req,res) =>{
-    res.send({message: 'Hello Express!'})
+app.get('/api/customers',(req,res)=>{
+    res.send([
+        {
+            'id':1,
+            'image':'https://placeimag.com/65/65/1',
+            'name':'유경동',
+            'birthday':'961221'         
+        },
+        {
+            'id':2,
+            'image':'https://placeimag.com/65/65/2',
+            'name':'은희자',
+            'birthday':'621017'  
+        }
+    ]);
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
